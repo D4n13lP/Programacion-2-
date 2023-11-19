83fccb5dacab94fc24dc5b82474ae6c081916888
D4n13lP <dan137p1n3d4@gmail.com>
2023-11-19 17:56:52 -0600

    Cambios en aplicacionCursos.java

--- Tercer_Semestre/POO/Cursos/AplicacionCursos.java
+++ Tercer_Semestre/POO/Cursos/AplicacionCursos.java
@@ -1,14 +1,10 @@
 //Daniel Pineda Ortega
 //package Daniel;
+import java.util.*; 
 
 public class AplicacionCursos {
 
-    //Llenado de cursos
-    //cursos=new ArrayList<Curso>();
-
-    Curso cursoTmp=new Curso(101, 100, "Programacion 1");
-    //cursos.add(cursoTmp);
-    
+   
     private Integer posicionAlumnoActual;
 
     //Constructor con todos lo parametros -----------------
@@ -41,5 +37,43 @@ public class AplicacionCursos {
     public Alumno getAlumnoActual(){
         return AlumnoActual;
     }
+
+
+    //-******************************************************************************************************************************************************
+     //Llenado de cursos
+    List<Curso>cursos=new ArrayList<Curso>();
+
+    Curso cursoTmp=new Curso(101, 100, "Programacion 1");
+    cursos.add(cursoTmp);
+
+    cursoTmp=new Curso(102, 90, "Diseño 1");
+    cursos.add(cursoTmp);
+
+    cursoTmp=new Curso(103, 80, "Programacion 2");
+    cursos.add(cursoTmp);
+
+    cursoTmp=new Curso(104, 50, "Redes");
+    cursos.add(cursoTmp);
+
+    cursoTmp=new Curso(105, 95, "Diseño Base de datos");
+    cursos.add(cursoTmp);
+
+    //Llenado de profesores
+    profesores=new ArrayList<Profesor>();
+    //TODO: Llenar 5 profesores y agregarlos a la lista
+
+    //Llenado de alumnos
+    alumnos=new ArrayList<Alumno>();
+
+    Alumno alumnoTmp=new Alumno("mujer.png", "101010", "9", "Karla", "Romero", "Cruz", Genero.FEMENINO, new Date());
+    alumnos.add(alumnoTmp);
+
+    alumnoTmp=new Alumno("hombre.png", "101010", "9", "Ignacio", "Luna", "Rojas", Genero.MASCULINO, new Date());
+    alumnos.add(alumnoTmp);
+
+    //TODO: Llenar 8 alumnos y agregarlos a la lista 
+
+    //establece la posicion actual
+    posicionAlumnoActual=0;
     
 }
